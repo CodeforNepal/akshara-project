e49714362b5360482f92ef7c47593c6ce6cd97a7
anupdhml <anupdhml+git@gmail.com>
2018-03-31 23:08:09 -0400

    attempt to index nepali json

--- index.sh
+++ index.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#
+# akshara_index.sh
+#
+# Simple script to index json documents to elasticsearch
+
+# exit the script when a command fails
+set -o errexit
+
+# cacth exit status for piped commands
+set -o pipefail
+
+if [ $# -eq 0 ]; then
+  echo "Please supply files to index"
+  exit 1
+fi
+
+##############################################################################
 
 INDEX="nepali"
 
@@ -8,12 +25,22 @@ PIPELINE="default_pipeline"
 # as unix time
 #CURRENT_TIME=$(date +'%s')
 
-DOC='{
-  "author": "test author",
-  "title": "test title",
-  "text": "test text"
-}'
+# test
+#content='{
+  #"author": "test author",
+  #"title": "test title",
+  #"text": "test text"
+#}'
 
-curl -XPOST "${ELASTICSEARCH_URL}/${INDEX}/_doc/?pipeline=${PIPELINE}&pretty" \
-  --header "Content-Type: application/json" \
-  --data "$DOC"
+##############################################################################
+
+for file in "$@"; do
+  echo "Indexing ${file}..."
+
+  content=$(<"$file")
+  #echo "$content"
+
+  curl -XPOST "${ELASTICSEARCH_URL}/${INDEX}/_doc/?pipeline=${PIPELINE}&pretty" \
+    --header "Content-Type: application/json" \
+    --data "$content"
+done
