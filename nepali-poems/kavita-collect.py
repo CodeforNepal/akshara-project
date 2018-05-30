ec1ea5159763bf5c760ad343991fca367725a7e8
ijesh <giriijesh@gmail.com>
2018-05-30 10:24:41 -0700

    Consolidating schema between sources

--- nepali-poems/kavita-collect.py
+++ nepali-poems/kavita-collect.py
@@ -4,9 +4,11 @@ from lxml import html
 import requests
 import os
 import signal
+import json
 import sys
 
 from time import sleep
+from utils import detect_lang
 
 global ctrlc
 ctrlc = False
@@ -26,10 +28,13 @@ for linkElem in links:
     if ctrlc:
         quit()
     else:
+        data = {}
         author, link = linkElem.xpath('./a/@title')[0], linkElem.xpath('./a/@href')[0]
+        data['author'] = author
+        data['genre'] = "कविता"
         author_page = requests.get(website + link)
         poem_links = html.fromstring(author_page.content).xpath('//div[@id="mw-content-text"]/ul/li/a')
-        dir = os.path.join("data", author)
+        dir = os.path.join("kavitakosh", author)
         if not os.path.exists(dir):
             os.makedirs(dir)
         # print(author)
@@ -39,10 +44,20 @@ for linkElem in links:
                 quit()
             else:
                 link2, title = poem_link.xpath('./@href')[0], poem_link.xpath('./@title')[0]
+                data['title'] = title
                 poem_page = requests.get(website + link2)
                 poem = html.fromstring(poem_page.content).xpath('//div[@class="poem"]/p/text()')
-                # print("".join(poem))
                 poem_str = "".join(poem).strip()
                 if poem_str:
+                    data['text'] = poem_str
+                    lang = detect_lang(poem_str)
+                    if lang == "ne":
+                        data['lang'] = "नेपाली"
+                    else:
+                        with open(os.path.join(dir, "undetected.txt"), "a") as f:
+                            f.write(lang + "\n")
+                            f.write(poem_str + "\n")
+                        continue
+
                     with open(os.path.join(dir, title.split('/')[0]) + '.txt', "w") as f:
-                        f.write(poem_str)
+                        f.write(json.dumps(data))
