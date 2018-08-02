478a45ffebc60720aeb56299361cacee5c7b2b37
Prasanna Suman <prasanna.tux@gmail.com>
2018-08-02 15:39:24 -0700

    feat: new logo component and minor style fixes

--- parijat-frontend/src/routes/home/index.js
+++ parijat-frontend/src/routes/home/index.js
@@ -3,13 +3,16 @@ import 'preact-material-components/Button/style.css';
 import { route } from 'preact-router';
 import SearchBox from '../../components/searchbox';
 import Footer from '../../components/footer';
+import Logo from '../../components/logo';
 import style from './style';
 
 export default class Home extends Component {
 	render() {
 		return (
 			<div class={style.Home}>
-				<div class={style.Logo}>सङ्ग्रह</div>
+				<div class={style.Logo}>
+					<Logo />
+				</div>
 				<div class={style.SearchBox}>
 					<SearchBox
 						fields={['title', 'author']}
