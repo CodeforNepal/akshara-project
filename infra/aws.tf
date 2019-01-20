a0046d390b84b1403f097763d4176330fc58e237
Aish Raj Dahal <aish@aishraj.com>
2019-01-19 20:06:19 -0800

    Remove nginx from containers

---
+++ infra/aws.tf
@@ -0,0 +1,17 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "us-east-1"
+}
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+
+resource "aws_instance" "example" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+}
