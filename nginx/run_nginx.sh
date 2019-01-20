b8a708eb098311671f4d2061d3e95054d029f58a
Aish Raj Dahal <aish@aishraj.com>
2019-01-19 23:00:43 -0800

    Change ports

--- nginx/run_nginx.sh
+++ nginx/run_nginx.sh
@@ -8,7 +8,9 @@ echo "Starting nginx..."
 # hack to use a templatized nginx config (without breaking vars in nginx itself)
 # See: https://serverfault.com/questions/577370/how-can-i-use-environment-variables-in-nginx-conf/582794
 export DOLLAR='$'
+export NGINX_SERVER_NAME=sangraha.org
+export NGINX_PORT=80
 
-envsubst < /etc/nginx/conf.d/akshara.template > /etc/nginx/conf.d/default.conf
+envsubst < ./akshara.template > /etc/nginx/conf.d/default.conf
 
 nginx -g "daemon off;"
