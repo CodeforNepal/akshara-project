659282e2218f78c4463b412b8395779239f7614e
tux4 <prasanna.tux@gmail.com>
2020-08-04 20:07:30 +0545

    feat: add link to bull-queue

--- parijat-frontend/src/components/admin/AdminTaskQueue.js
+++ parijat-frontend/src/components/admin/AdminTaskQueue.js
@@ -1,11 +1,14 @@
 import { h } from 'preact';
+import { BHUPI_ENDPOINT } from '../../api';
 
 function AdminTask() {
   return (
     <div>
       <h4>Long Running Tasks</h4>
-      <p>Here you can monitor long running server tasks.
-         This feature is not implemented yet.</p>
+      <p>Here you can monitor long running server tasks. This feature is not
+      implemented yet. Meanwhile use <a target="_blank" href={`${BHUPI_ENDPOINT}admin/queues`}>bull-board </a>
+      for task monitoring.
+      </p>
     </div>
   );
 }
